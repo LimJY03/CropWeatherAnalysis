fce81f510821b07509cd96a2d46e357b8ce9ca83
Lim JY <limjunyi03@gmail.com>
2024-12-27 17:06:03 +0000

    Added constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+WEATHER_FEATURES = {
+    'Wind Speed at 10 Meters (m/s)': 'WS10M',
+    'Surface Soil Wetness': 'GWETTOP',
+    'Root Zone Soil Wetness': 'GWETROOT',
+    'Cloud Amount (%)': 'CLOUD_AMT',
+    'Temperature at 2 Meters Range (C)': 'T2M_RANGE',
+    'Rainfall (mm/day)': 'PRECTOTCORR',
+    'Rainfall Days': 'PREC_DAYS'
+}
