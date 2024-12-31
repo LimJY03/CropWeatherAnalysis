e1cd4abea1dda052b034f541d47a421d32b414e9
Lim JY <limjunyi03@gmail.com>
2024-12-31 18:09:09 +0000

    Adjust path to pages

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import streamlit as st
 st.set_page_config(layout='wide')
 
 # Paging
-dashboard_page = st.Page('dashboard.py', title='Dashboard', icon=':material/monitoring:')
-prediction_page = st.Page('prediction.py', title='Prediction', icon=':material/psychology:')
-manual_page = st.Page('manual.py', title='User Manual', icon=':material/menu_book:')
+dashboard_page = st.Page('pages/dashboard.py', title='Visualization', icon=':material/monitoring:')
+prediction_page = st.Page('pages/prediction.py', title='Prediction', icon=':material/psychology:')
+manual_page = st.Page('pages/manual.py', title='User Manual', icon=':material/menu_book:')
 pg = st.navigation([dashboard_page, prediction_page, manual_page])
 pg.run()
 
